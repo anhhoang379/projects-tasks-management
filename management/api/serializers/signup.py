af0c8849a7293058fdc22cc60a4e4e00cbddfd20
hoanglb <hoangtoei@gmail.com>
2023-12-15 14:48:06 +0700

    create staff

--- management/api/serializers/signup.py
+++ management/api/serializers/signup.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=True, style={"input_type": "password"}
     )
-    
+
     class Meta:
         model = User
         fields = ("username", "password")
